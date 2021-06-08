ba9b80db2aa93bc2dff9802ae6e8ff7c1dc7c80d
Daniil Lebedev <daniilwork247@gmail.com>
2021-06-08 18:25:14 +0100

    Website version 1.0

--- oursite/settings.py
+++ oursite/settings.py
@@ -17,6 +17,7 @@ import dj_database_url
 from decouple import config
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
 	#all paths for pages app should be written here.
 
-	path('', views.aboutPage, name="aboutPage")
+	path('', views.aboutPage, name="aboutPage"),
+
+	#path for coming soon page
+	path('coming-soon', views.comingSoonPage, name="comingSoonPage")
 ]
