e9fad88a1481d1f4463ddce3c31090c9bc23ac70
Daniil Lebedev <daniilwork247@gmail.com>
2021-07-26 16:44:44 +0100

    Added new packages for downloading the certificates

--- daniil/urls.py
+++ daniil/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dannilPage, name='dannilPage')
+    path('', views.dannilPage, name='dannilPage'),
+
+    #path for exporting a test document
+    path('export-pdf', views.exportCertificate, name='export-pdf')
 ]
